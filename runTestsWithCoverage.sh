245be3e8929f4694d1ad01af459d082a886480e1
Jade <jadeadedokun@gmail.com>
2025-03-21 09:35:58 +0000

    Small Tweaks including updated bash file and README

--- runTestsWithCoverage.sh
+++ runTestsWithCoverage.sh
@@ -8,4 +8,4 @@ pip3 install coverage
 coverage run -m unittest test_for_algebraic_calculations.py test_for_inverted_integer.py test_for_inverted_integers.py test_for_inverted_roots_of_unity.py
 
 # Generates a coverage report
-coverage report
+coverage report test_for_algebraic_calculations.py test_for_inverted_integer.py test_for_inverted_integers.py test_for_inverted_roots_of_unity.py
